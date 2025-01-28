b502fd920f6a417466d5d6687c1076e96346b314
Sumitmewada248 <sumitmewa@gmail.com>
2025-01-28 15:28:21 +0530

    new practice

---
+++ New folder/src/App.jsx
@@ -0,0 +1,19 @@
+
+const crs=<ol>
+<li>php</li>
+<li>java</li>
+<li>python</li>
+<li>ruby</li>
+</ol>
+
+
+const App=()=>{
+  return(
+    <>
+    <p>Our Courses{crs}</p>
+    
+    </>
+
+  )
+}
+export default App;
