ec3b5cc5f3812976a4b99942383383074806029f
Sumitmewada248 <sumitmewa@gmail.com>
2025-01-30 14:52:59 +0530

    props

---
+++ jan30/src/App.jsx
@@ -0,0 +1,11 @@
+import College from "./College";
+
+const App=()=>{
+  return(
+    <>
+    <h1>Welcome</h1>
+    <College clg="VNS Group" fee="556545"/>
+    </>
+  )
+}
+export default App;
---
+++ jan30/src/College.jsx
@@ -0,0 +1,9 @@
+const College = (props) => {
+    return (
+        <>
+            <h1> My College Name:{props.clg} </h1>
+            <h1>My Fees:{props.fee} </h1>
+        </>
+    );
+};
+export default College;
