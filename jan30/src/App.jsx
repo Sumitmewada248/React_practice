6f5d29a433fa8f23ddc5c5374e3917e8cd4103e8
Sumitmewada248 <sumitmewa@gmail.com>
2025-02-03 21:31:46 +0530

    design

--- jan30/src/App.jsx
+++ jan30/src/App.jsx
@@ -5,6 +5,8 @@
 // const coll="IIT"
 // const fees="50000"
 
+import EmpDesign from "./EmpDesign"
+
 // const App=()=>{
 //   return(
 //     <>
@@ -113,13 +115,74 @@
 // }
 // export default App;
 
+// const App=()=>{
+//   return(
+//     <>
+//     <div style={{width:"300px",border:"1px solid black",margin:"auto",padding:"10px",textAlign:"center",height:"150px",backgroundColor:"lightblue",borderRadius:"20px",marginTop:"300px"}}>
+//       <div style={{border:"1px solid red",width:"250px",textAlign:"center",margin:"auto",height:"90px",paddingTop:"20px",backgroundColor:"yellow",borderRadius:"20px",marginTop:"20px"}}><h3 style={{marginTop:"30px",fontSize:"25px"}}>CYBROM</h3></div>
+//     </div>
+//     </>
+//   )
+// }
+// export default App;
+
+// import EmpData from "./EmpData"
+// import EmpDesign from "./EmpDesign"
+
+// const App=()=>{
+  
+//   const ans=EmpData.map((key)=><EmpDesign
+//   name={key.name} rollno={key.rollno} designation={key.designation} salary={key.salary}/>
+//   )
+
+//   return(
+
+//     <>
+//     <h1>React App</h1>
+
+//     <table border={2}>
+//       <tr>
+//         <th>ID</th>
+//         <th>Name</th>
+//         <th>Designation</th>
+//         <th>salary</th>
+//       </tr>
+//       {ans}
+//     </table>
+//     </>
+
+//   )
+
+// }
+
+// export default App
+
+import EmpData from "./EmpData"
+
+
+
 const App=()=>{
+
+  const ans=EmpData.map((key)=><EmpDesign
+  rollno={key.rollno} name={key.name}  designation={key.designation} salary={key.salary}/>
+  
+  )
+
   return(
-    <>
-    <div style={{width:"300px",border:"1px solid black",margin:"auto",padding:"10px",textAlign:"center",height:"150px",backgroundColor:"lightblue",borderRadius:"20px"}}>
-      <div style={{border:"1px solid red",width:"250px",textAlign:"center",margin:"auto",height:"90px",paddingTop:"20px",backgroundColor:"yellow",borderRadius:"20px",marginTop:"20px"}}><h3 style={{marginTop:"30px",fontSize:"25px"}}>CYBROM</h3></div>
-    </div>
-    </>
+   <>
+   <h1>React App</h1>
+   <table border={2}>
+    <tr>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Designation</th>
+      <th>Salary</th>
+    </tr>
+    {ans}
+   </table>
+   
+   </>
+
   )
 }
-export default App;
+export default App
--- jan30/src/EmpDesign.jsx
+++ jan30/src/EmpDesign.jsx
@@ -1,16 +1,34 @@
-const EmpDesign = (props) => {
-    return (
-        <>
+// const EmpDesign = (props) => {
+//     return (
+//         <>
             
-            <tr  bgcolor="lightblue">
-            <td>{props.rollno}</td>
-                <td>{props.name}</td>
+//             <tr  bgcolor="lightblue">
+//             <td>{props.rollno}</td>
+//                 <td>{props.name}</td>
              
-                <td>{props.designation}</td>
-                <td>{props.salary}</td>
-            </tr>
-        </>
+//                 <td>{props.designation}</td>
+//                 <td>{props.salary}</td>
+//             </tr>
+//         </>
+//     )
+// }
+
+// export default EmpDesign
+
+
+const EmpDesign=(props)=>{
+    return(
+    <>
+    <tr>
+        <td>{props.rollno}</td>
+        <td>{props.name}</td>
+        <td>{props.designation}</td>
+        <td>{props.salary}</td>
+    </tr>
+    
+    </>
+
+
     )
 }
-
 export default EmpDesign
